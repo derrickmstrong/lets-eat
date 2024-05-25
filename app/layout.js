e797f806ecd1d03c83b0e0c94ca2abdee09512f1
Derrick Strong <derrickmstrong@gmail.com>
2024-05-25 14:34:29 -0500

    Add layout example

--- app/layout.js
+++ app/layout.js
@@ -1,3 +1,4 @@
+import Header from '@/components/main-header';
 import './globals.css';
 
 export const metadata = {
@@ -9,6 +10,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body>
+        <Header />
         <div className="header-background">
           <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
             <defs>
---
+++ app/meals/layout.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const MealsLayout = ({ children }) => {
+  return (
+    <main>
+      <h1>Meals Layout</h1>
+      {children}
+    </main>
+  );
+};
+
+export default MealsLayout;
