79a5fd24871a6420523a934e9ae82c76dec080ac
Derrick Strong <derrickmstrong@gmail.com>
2024-05-26 07:01:46 -0500

    Add loading state

---
+++ app/meals/loading.js
@@ -0,0 +1,9 @@
+import styles from "./loading.module.css";
+
+const MealsLoading = () => {
+  return (
+    <p className={styles.loading}>Fetching meals...</p>
+  )
+}
+
+export default MealsLoading
