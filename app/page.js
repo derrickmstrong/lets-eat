6238abb16f897c6b09b967584689bc3b4daca9d2
Derrick Strong <derrickmstrong@gmail.com>
2024-05-26 05:19:35 -0500

    Update page layout and add main header component and slideshow

--- app/page.js
+++ app/page.js
@@ -1,11 +1,54 @@
+import Link from "next/link";
+
+import styles from "./page.module.css";
+import ImageSlideshow from "@/components/images/image-slideshow";
 
 const Home = () => {
   return (
-    <main>
-      <h1 style={{ color: 'white', textAlign: 'center' }}>
-        Time to get started!
-      </h1>
-    </main>
+    <>
+      <header className={styles.header}>
+        <div className={styles.slideshow}>
+          <ImageSlideshow />
+        </div>
+        <div>
+          <div className={styles.hero}>
+            <h1>Let&apos;s Eat for Foodies</h1>
+            <p>Taste & share food from all over the world.</p>
+          </div>
+          <div className={styles.cta}>
+            <Link href="/community">Join the Community</Link>
+            <Link href="/meals">Explore Meals</Link>
+          </div>
+        </div>
+      </header>
+      <main>
+        <section className={styles.section}>
+          <h2>How it works</h2>
+          <p>
+            Let&apos;s Eat is a platform for foodies to share their favorite
+            recipes with the world. It&apos;s a place to discover new dishes,
+            and to connect with other food lovers.
+          </p>
+          <p>
+            Let&apos;s Eat is a place to discover new dishes, and to connect
+            with other food lovers.
+          </p>
+        </section>
+
+        <section className={styles.section}>
+          <h2>Why Let&apos;s Eat?</h2>
+          <p>
+            Let&apos;s Eat is a platform for foodies to share their favorite
+            recipes with the world. It&apos;s a place to discover new dishes,
+            and to connect with other food lovers.
+          </p>
+          <p>
+            Let&apos;s Eat is a place to discover new dishes, and to connect
+            with other food lovers.
+          </p>
+        </section>
+      </main>
+    </>
   );
 }
 
