ab2ff012e774f586cae9332f9b7840945b50cf21
Derrick Strong <derrickmstrong@gmail.com>
2024-05-26 16:14:14 -0500

    Add meal details

--- app/meals/[slug]/page.js
+++ app/meals/[slug]/page.js
@@ -1,11 +1,44 @@
-import React from 'react'
+import Image from "next/image";
+import styles from "./page.module.css";
+import { getMeal } from "@/lib/meals";
+import { notFound } from "next/navigation";
 
-const MealsDetails = ({ params }) => {
+const MealsDetails = async ({ params }) => {
+  const meal = await getMeal(params.slug);
+
+  if (!meal) {
+    notFound();
+  }
+  
   return (
-    <div>
-        <h1>{params.slug}</h1>
-    </div>
-  )
-}
+    <>
+      <header className={styles.header}>
+        <div className={styles.image}>
+          <Image
+            src={meal.image}
+            alt={meal.title}
+            sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
+            fill
+          />
+        </div>
+        <div className={styles.headerText}>
+          <h1>{meal.title}</h1>
+          <p className={styles.creator}>
+            by <a href={`mailto:${meal.creator_email}`}>{meal.creator}</a>
+          </p>
+          <p className={styles.summary}>{meal.summary}</p>
+        </div>
+      </header>
+      <main>
+        <p
+          className={styles.instructions}
+          dangerouslySetInnerHTML={{
+            __html: meal.instructions.replace(/\n/g, `<br />`), // Convert newlines to <br> tags
+          }}
+        ></p>
+      </main>
+    </>
+  );
+};
 
-export default MealsDetails
+export default MealsDetails;
